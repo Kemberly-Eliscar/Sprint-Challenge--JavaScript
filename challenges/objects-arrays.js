fefde82938d3de142b658a0e754740c9bec1c97b
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2019-06-14 14:08:48 -0400

    Almost done with objects file

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -131,8 +131,13 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-let animalNames = zooAnimals.forEach(zooAnimals, );
-console.log(animalNames);
+
+let animal = [];
+
+ zooAnimals.forEach(function(i) {
+   animal.push(`${i.animal_name} ${i.scientific_name}`);
+ });
+ console.log(animalNames);
 
 /* Request 2: .map()    
 
